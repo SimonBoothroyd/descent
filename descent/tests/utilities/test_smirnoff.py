9acdf6ebc487a79ddea01dc0c6428896d463fe40
SimonBoothroyd <simon.boothroyd@hotmail.com>
2021-09-13 14:32:38 +0100

    Add utility to find SMIRNOFF parameters exercised by dataset

--- descent/tests/utilities/test_smirnoff.py
+++ descent/tests/utilities/test_smirnoff.py
@@ -1,10 +1,12 @@
+import pytest
 import torch
 from openff.interchange.models import PotentialKey
 from openff.toolkit.typing.engines.smirnoff import ForceField
 from simtk import unit as simtk_unit
 
+from descent.data import DatasetEntry
 from descent.tests import is_close
-from descent.utilities.smirnoff import perturb_force_field
+from descent.utilities.smirnoff import exercised_parameters, perturb_force_field
 
 
 def test_perturb_force_field():
@@ -40,3 +42,193 @@ def test_perturb_force_field():
     assert is_close(
         perturbed_force_field["ProperTorsions"].parameters[smirks].idivf2, 1.2
     )
+
+
+@pytest.mark.parametrize(
+    "handlers_to_include,"
+    "handlers_to_exclude,"
+    "ids_to_include,"
+    "ids_to_exclude,"
+    "attributes_to_include,"
+    "attributes_to_exclude,"
+    "n_expected,"
+    "expected_handlers,"
+    "expected_potential_keys,"
+    "expected_attributes",
+    [
+        (
+            None,
+            None,
+            None,
+            None,
+            None,
+            None,
+            36,
+            {"Bonds", "Angles"},
+            {
+                PotentialKey(id=smirks, mult=mult, associated_handler=handler)
+                for smirks in ("a", "b", "c")
+                for mult in (None, 0, 1)
+                for handler in ("Bonds", "Angles")
+            },
+            {"k", "length", "angle"},
+        ),
+        (
+            ["Bonds"],
+            None,
+            None,
+            None,
+            None,
+            None,
+            18,
+            {"Bonds"},
+            {
+                PotentialKey(id=smirks, mult=mult, associated_handler="Bonds")
+                for smirks in ("a", "b", "c")
+                for mult in (None, 0, 1)
+            },
+            {"k", "length"},
+        ),
+        (
+            None,
+            ["Bonds"],
+            None,
+            None,
+            None,
+            None,
+            18,
+            {"Angles"},
+            {
+                PotentialKey(id=smirks, mult=mult, associated_handler="Angles")
+                for smirks in ("a", "b", "c")
+                for mult in (None, 0, 1)
+            },
+            {"k", "angle"},
+        ),
+        (
+            None,
+            None,
+            [PotentialKey(id="b", mult=0, associated_handler="Bonds")],
+            None,
+            None,
+            None,
+            2,
+            {"Bonds"},
+            {PotentialKey(id="b", mult=0, associated_handler="Bonds")},
+            {"k", "length"},
+        ),
+        (
+            None,
+            None,
+            None,
+            [
+                PotentialKey(id="b", mult=0, associated_handler="Bonds"),
+            ],
+            None,
+            None,
+            34,
+            {"Bonds", "Angles"},
+            {
+                PotentialKey(id=smirks, mult=mult, associated_handler=handler)
+                for handler in ("Bonds", "Angles")
+                for smirks in ("a", "b", "c")
+                for mult in (None, 0, 1)
+                if (smirks != "b" or mult != 0 or handler != "Bonds")
+            },
+            {"k", "length", "angle"},
+        ),
+        (
+            None,
+            None,
+            None,
+            None,
+            ["length"],
+            None,
+            9,
+            {"Bonds"},
+            {
+                PotentialKey(id=smirks, mult=mult, associated_handler="Bonds")
+                for smirks in ("a", "b", "c")
+                for mult in (None, 0, 1)
+            },
+            {"length"},
+        ),
+        (
+            None,
+            None,
+            None,
+            None,
+            None,
+            ["length"],
+            27,
+            {"Bonds", "Angles"},
+            {
+                PotentialKey(id=smirks, mult=mult, associated_handler=handler)
+                for handler in ("Bonds", "Angles")
+                for smirks in ("a", "b", "c")
+                for mult in (None, 0, 1)
+            },
+            {"k", "angle"},
+        ),
+    ],
+)
+def test_exercised_parameters(
+    handlers_to_include,
+    handlers_to_exclude,
+    ids_to_include,
+    ids_to_exclude,
+    attributes_to_include,
+    attributes_to_exclude,
+    n_expected,
+    expected_handlers,
+    expected_potential_keys,
+    expected_attributes,
+):
+    class MockEntry(DatasetEntry):
+        def evaluate_loss(self, model, **kwargs):
+            pass
+
+    def mock_entry(handler, patterns, mult):
+
+        attributes = {"Bonds": ["k", "length"], "Angles": ["k", "angle"]}[handler]
+
+        entry = MockEntry.__new__(MockEntry)
+        entry._model_input = {
+            (handler, ""): (
+                None,
+                None,
+                [
+                    (
+                        PotentialKey(id=smirks, mult=mult, associated_handler=handler),
+                        attributes,
+                    )
+                    for smirks in patterns
+                ],
+            )
+        }
+        return entry
+
+    entries = [
+        mock_entry(handler, patterns, mult)
+        for handler in ["Bonds", "Angles"]
+        for patterns in [("a", "b"), ("b", "c")]
+        for mult in [None, 0, 1]
+    ]
+
+    parameter_keys = exercised_parameters(
+        entries,
+        handlers_to_include,
+        handlers_to_exclude,
+        ids_to_include,
+        ids_to_exclude,
+        attributes_to_include,
+        attributes_to_exclude,
+    )
+
+    assert len(parameter_keys) == n_expected
+
+    actual_handlers, actual_keys, actual_attributes = zip(*parameter_keys)
+
+    assert {*actual_handlers} == expected_handlers
+    assert {*actual_keys} == expected_potential_keys
+    assert {*actual_attributes} == expected_attributes
