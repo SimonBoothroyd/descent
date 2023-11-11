33b4ed475843516b9f4458964453391433fa8544
SimonBoothroyd <simon.boothroyd@hotmail.com>
2023-11-11 12:15:54 -0500

    Fix test

--- descent/tests/utils/test_dataset.py
+++ descent/tests/utils/test_dataset.py
@@ -7,14 +7,12 @@ def test_iter_dataset():
     test_dataset = datasets.Dataset.from_dict(
         {"column1": ["data1", "data2", "data3"], "column2": ["data4", "data5", "data6"]}
     )
-    columns = ["column1", "column2"]
-
     expected_output = [
         {"column1": "data1", "column2": "data4"},
         {"column1": "data2", "column2": "data5"},
         {"column1": "data3", "column2": "data6"},
     ]
 
-    output = list(iter_dataset(test_dataset, columns))
+    output = list(iter_dataset(test_dataset))
 
     assert output == expected_output
